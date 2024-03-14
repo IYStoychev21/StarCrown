eafb63a410dd6a819487c1036475f8809f1a35fd
IYStoychev21 <IYStoychev21@codingburgas.bg>
2024-03-14 09:29:14 +0200

    fix(api): middleware

--- api/cmd/server/main.go
+++ api/cmd/server/main.go
@@ -1,30 +1,40 @@
 package main
 
 import (
-    "log"
-    
-    "github.com/IYStoychev21/star-crown/api/internal/handlers"
+	"log"
+	"net/http"
 
-    "github.com/gin-gonic/gin"
-    "github.com/joho/godotenv"
+	"github.com/IYStoychev21/star-crown/api/internal/handlers"
+
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-    err := godotenv.Load()
-    if err != nil {
-        log.Fatal("Error loading .env file") 
-    }
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
-    router := gin.Default()
+	router := gin.Default()
 
-    //router.Use(authMiddleware)
+	router.GET("/", handlers.IndexHandler)
 
-    router.GET("/", handlers.IndexHandler)
+	router.GET("/login", handlers.HandleLoging)
+	router.GET("/auth/google/callback", handlers.HandleGoogleCallback)
+	router.GET("/protected", authMiddleware, handlers.ProtectedHandler)
 
-    router.GET("/login", handlers.HandleLoging)
-    router.GET("/auth/google/callback", handlers.HandleGoogleCallback)
-    router.GET("/protected", handlers.ProtectedHandler)
-
-    router.Run(":3000")
+	router.Run(":3000")
 }
 
+func authMiddleware(c *gin.Context) {
+	_, err := c.Request.Cookie("access_token")
+
+	if err != nil {
+		c.Redirect(http.StatusFound, "/login")
+		c.Abort()
+		return
+	}
+
+	c.Next()
+}
--- api/internal/handlers/auth.go
+++ api/internal/handlers/auth.go
@@ -1,50 +1,47 @@
-
 package handlers
 
 import (
-    "net/http"
-    "os"
-    "fmt"
+	"net/http"
+	"os"
 
-    "github.com/gin-gonic/gin"
-    "golang.org/x/oauth2"
-    "golang.org/x/oauth2/google"
+	"github.com/gin-gonic/gin"
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/google"
 )
 
 func setupGoogleOAuthConfig() *oauth2.Config {
-    return &oauth2.Config{
-        RedirectURL:  os.Getenv("REDIRECTURL"),
-        ClientID:     os.Getenv("CLIENTID"),
-        ClientSecret: os.Getenv("CLIENTSECRET"),
-        Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/drive.file"},
-        Endpoint:     google.Endpoint,
-    }
+	return &oauth2.Config{
+		RedirectURL:  os.Getenv("REDIRECTURL"),
+		ClientID:     os.Getenv("CLIENTID"),
+		ClientSecret: os.Getenv("CLIENTSECRET"),
+		Scopes:       []string{"https://www.googleapis.com/auth/userinfo.email", "https://www.googleapis.com/auth/drive.file"},
+		Endpoint:     google.Endpoint,
+	}
 }
 
 func HandleLoging(c *gin.Context) {
-    googleOauthConfig := setupGoogleOAuthConfig()
+	googleOauthConfig := setupGoogleOAuthConfig()
 
-    url := googleOauthConfig.AuthCodeURL(os.Getenv("OAUTHSTATESTRING"))
-    c.Redirect(http.StatusTemporaryRedirect, url)
+	url := googleOauthConfig.AuthCodeURL(os.Getenv("OAUTHSTATESTRING"))
+	c.Redirect(http.StatusTemporaryRedirect, url)
 }
 
 func HandleGoogleCallback(c *gin.Context) {
-    code := c.Query("code")
+	code := c.Query("code")
 
-    googleOauthConfig := setupGoogleOAuthConfig()
+	googleOauthConfig := setupGoogleOAuthConfig()
 
-    token, err := googleOauthConfig.Exchange(c, code)
-    if err != nil {
-        c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to exchange token"})
-        return
-    }
+	token, err := googleOauthConfig.Exchange(c, code)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to exchange token"})
+		return
+	}
 
-    c.SetCookie("access_token", token.AccessToken, 3600, "/", "", false, true)
+	c.SetCookie("access_token", token.AccessToken, 3600, "/", "", false, true)
 
-    c.Redirect(http.StatusFound, "/protected")
+	c.Redirect(http.StatusFound, "/protected")
 }
 
 func ProtectedHandler(c *gin.Context) {
-    c.JSON(http.StatusOK, gin.H{"message": "This is a protected route"})
+	c.JSON(http.StatusOK, gin.H{"message": "This is a protected route"})
 }
-
