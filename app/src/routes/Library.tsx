f7812f8c71c698703a70e107e4ce7dc8b8304c4a
IYStoychev21 <IYStoychev21@codingburgas.bg>
2024-03-25 19:52:45 +0200

    feat(app): remove nav

--- app/src/routes/Library.tsx
+++ app/src/routes/Library.tsx
@@ -1,8 +1,6 @@
-import Nav from '../components/Nav'
 import { userAPI } from '@/apis/userAPI'
 import { gamesAPI } from '@/apis/gamesAPI'
 import { useState, useEffect } from 'react'
-import { Separator } from '@/components/ui/separator'
 import { UserType, GameType, GameSaveType} from '@/shared.types'
 import {
   Dialog,
@@ -104,13 +102,7 @@ export default function Library() {
     return (
         <>
             <div className='min-w-screen min-h-screen flex flex-col'>
-                <Nav avaratImage={userInfo?.pic} userName={userInfo?.name}/>
-
-                <div className='w-full flex justify-center'>
-                    <Separator className='w-[95%] bg-slate-500' />
-                </div>
-
-                <div className='text-white font-bold ml-8 mt-3 flex flex-col gap3'>
+                <div className='text-white font-bold ml-8 mt-8 flex flex-col gap3'>
                     <h1 className='text-3xl'>Your Save Library</h1>
 
                     <div className={`mt-20 grid ${ convertNumberToCol(Math.floor(window.innerWidth / 230))} gap-5`}>
